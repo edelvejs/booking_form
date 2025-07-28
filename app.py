3f96e3b074464dbf60721949753e84127dbe1616
edelvejs <edelvejs.transfer@gmail.com>
2025-07-29 00:58:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def admin_view():
     return render_template("admin.html", data_sorted=data_sorted)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000')
+    app.run(host='0.0.0.0', port=10000)
